aa69ac334e177e524bb84b2ac55cd457008823a3
nxtio <200070253+nxtio@users.noreply.github.com>
2025-05-23 20:37:27 +0700

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,17 @@
+local infgem = {
+	"WalkSpeed",
+	-1e99
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("UpgradeEvent"):FireServer(unpack(infgem))
+
+for i = 1, 10 do
+  local givepet = {
+  	4
+  }
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(givepet))
+  local x10money = {
+  	8
+  }
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(x10money))
+  task.wait(2)
+end
