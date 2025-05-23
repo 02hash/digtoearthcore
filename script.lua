fce6ca9b7e40c508bdd7602873cbb4a3638d6047
nxtio <200070253+nxtio@users.noreply.github.com>
2025-05-23 23:52:21 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,19 +4,27 @@ local infgem = {
 }
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("UpgradeEvent"):FireServer(unpack(infgem))
 
+task.wait(0.5)
+
+local x10money = {
+  	8
+  }
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(x10money))
+
+task.wait(0.5)
+
+  local x10spins = {
+        10
+  }
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(x10spins))
+
+task.wait(0.5)
+
 for i = 1, 10 do
   local givepet = {
   	4
   }
   game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(givepet))
-  local x10money = {
-  	8
-  }
-  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(x10money))
-  local x10spins = {
-        10
-  }
-  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(x10spins))
 
   task.wait(2)
 end
