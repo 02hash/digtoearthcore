ada633ee7f34249fadd3f37f9f910e59818abbf9
ren <200070253+02hash@users.noreply.github.com>
2025-09-10 14:00:58 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,30 +1,4 @@
-local infgem = {
-	"WalkSpeed",
-	-1e99
-}
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("UpgradeEvent"):FireServer(unpack(infgem))
-
-task.wait(0.5)
-
-local x10money = {
-  	8
-  }
-  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(x10money))
-
-task.wait(0.5)
-
-  local x10spins = {
-        10
-  }
-  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(x10spins))
-
-task.wait(0.5)
-
-for i = 1, 10 do
-  local givepet = {
-  	4
-  }
-  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(givepet))
-
-  task.wait(2)
-end
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Dig To Earth Core",
+	Text = "Hey, this script is discontinued. - @zywaah on disc",
+})
